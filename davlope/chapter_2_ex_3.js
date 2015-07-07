b7df6a866bec5f49aa2d963a5d6c950861374c3a
David Lopez <davlope@gmail.com>
2015-07-07 20:16:14 +0200

    Ejercicios de tmeas dos y tres.

---
+++ davlope/chapter_2_ex_3.js
@@ -0,0 +1,19 @@
+var size = 15;
+var odd_patttern = "";
+var even_patttern = "";
+for(var i=0; i<size; i++){
+    if (i % 2 == 0){
+        odd_patttern += "#";
+        even_patttern += " ";
+    }else{
+        odd_patttern += " ";
+        even_patttern += "#";        
+    }
+}
+for(var i=0; i<size; i++){
+    if (i % 2 == 0){
+        console.log(even_patttern);
+    }else{
+        console.log(odd_patttern);
+    }
+}
---
+++ davlope/chapter_3_ex_3.js
@@ -0,0 +1,23 @@
+function countChar(s, c){
+     var char_ctr = 0;
+    for (var i = 0; i  < s.length - 1; i ++ ){
+        if (s.charAt(i) == c){
+            char_ctr++;
+        }
+    }
+    return char_ctr;   
+}
+function countBs(s){
+    return countChar(s, 'B');
+}
+console.log(countBs("sdfghjk"));
+console.log(countBs("DFGHJFKGH"));
+console.log(countBs("ABFGRUEYBTBRTBbbbbbbbb"));
+
+console.log(countChar("sdfghjkasdfgh", 'a'));
+console.log(countChar("DFGHJFKGH", 'F'));
+console.log(countChar("ABFGRUEYBTBRTBbbbbbbbb", 'b'));
+
+console.log(countChar("sdfghjk", 'B'));
+console.log(countChar("DFGHJFKGH", 'B'));
+console.log(countChar("ABFGRUEYBTBRTBbbbbbbbb", 'B'));
