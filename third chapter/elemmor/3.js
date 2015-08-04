8c41f1e4fa795b8be11f0f99ccfc030f094001a4
Javier Gamarra <nhpatt@gmail.com>
2015-08-04 18:35:28 +0200

    formatting

--- third chapter/elemmor/3.js
+++ third chapter/elemmor/3.js
@@ -1,37 +1,31 @@
-// Your code here.
-min = function (uno, otro){
-  if(uno <= otro){
-    return uno
-  }
-  return otro;
-}
-
-
-// Your code here.
-isEven = function (numero){
-  if(numero<0){
-  return   isEven(-numero);
-  }
-  if (numero==0){
-    return true;
-  }
-  if (numero==1){
-    return false;
-  }
-  return isEven(numero-2);
-  
+min = function (one, another) {
+    if (one <= another) {
+        return one;
+    }
+    return another;
 };
 
-countBs = function (cadena){
-  return countChar(cadena);
-}
-countChar = function (cadena, letra){
-  if(!letra){
-    letra = 'B'
-  }
-  var contador=0;
-  for (var i = 0; i < cadena.length; i++){
-    contador+=(cadena.charAt(i)==letra);
-  }  
-  return contador;
-}
+isEven = function (number) {
+    if (number < 0) {
+        return isEven(-number);
+    }
+    if (number == 0) {
+        return true;
+    }
+    if (number == 1) {
+        return false;
+    }
+    return isEven(number - 2);
+};
+
+countBs = function (text) {
+    return countChar(text, "B");
+};
+
+countChar = function (text, letter) {
+    var counter = 0;
+    for (var i = 0; i < text.length; i++) {
+        counter += (text.charAt(i) == letter);
+    }
+    return counter;
+};
