2893f6598213d8548ac1cda0544d31771491e8a6
Javier Gamarra <nhpatt@gmail.com>
2015-08-25 16:06:17 +0200

    missing return and useless var

--- fourth chapter/aliciagar2/reverseArrayInPlace.js
+++ fourth chapter/aliciagar2/reverseArrayInPlace.js
@@ -1,10 +1,14 @@
 var reverseArrayInPlace = function (arrayValue) {
-    var rango = [];
+
     var medio = Math.floor(arrayValue.length / 2);
-    console.log(medio);
+
     for (var i = 0; i < medio; i++) {
         var swap = arrayValue[i];
         arrayValue[i] = arrayValue[arrayValue.length - i - 1];
         arrayValue[arrayValue.length - i - 1] = swap;
     }
+    return arrayValue;
 };
+
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
+console.log(reverseArrayInPlace([1, 2, 3, 4]));
