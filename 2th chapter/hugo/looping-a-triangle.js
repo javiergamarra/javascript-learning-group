b3c593475e556b2c556afbca531c8ebb8d22ef42
Javier Gamarra <javier.gamarra@liferay.com>
2015-09-23 17:18:59 +0200

    Revert "Ejercicios hugo"

--- 2th chapter/hugo/looping-a-triangle.js
+++
@@ -1,9 +0,0 @@
-//Looping a triangle
-function printTriangle() {
-    var strToPrint = '#';
-    for (var i = 0; i < 7; i++) {
-        console.log(strToPrint);
-        strToPrint += '#';
-    }
-}
-exercise1();
--- 3th chapter/hugo/minimum.js
+++
@@ -1,7 +0,0 @@
-//Exercise minimum
-var min = function (a, b) {
-    return a == b || a > b ? b : a;
-}
-
-
-console.log('min(a,b): ', min('a', 'b'))
