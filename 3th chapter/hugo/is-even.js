66c0620805dc9f2c0776900c52a3dbb7813684b3
Javier Gamarra <nhpatt@gmail.com>
2015-09-23 16:49:47 +0200

    source formatting

--- 3th chapter/hugo/is-even.js
+++ 3th chapter/hugo/is-even.js
@@ -1,12 +1,12 @@
 //Exercise isEven recursively
-var isEven = function(number) {
-  if (number < 0)
-    return isEven(-number);
-  else if (number > 1)
-    return isEven(number - 2)
-  else {
-    return number == 0 ? true : false;
-  }
+var isEven = function (number) {
+    if (number < 0)
+        return isEven(-number);
+    else if (number > 1)
+        return isEven(number - 2)
+    else {
+        return number == 0 ? true : false;
+    }
 }
 
 console.log('isEven(17): ', isEven(17))
