599c949ae0fd8121ffa46aa6505b4828ba0799b0
Hugo Nebreda <hugo.nebreda@gmail.com>
2015-09-23 16:31:28 +0200

    Ejercicios Hugo Capitulo 2, 3 y 4

---
+++ 3th chapter/hugo/is-even.js
@@ -0,0 +1,13 @@
+//Exercise isEven recursively
+var isEven = function(number) {
+  if (number < 0)
+    return isEven(-number);
+  else if (number > 1)
+    return isEven(number - 2)
+  else {
+    return number == 0 ? true : false;
+  }
+}
+
+console.log('isEven(17): ', isEven(17))
+console.log('isEven(-8): ', isEven(-8))
---
+++ 4th chapter/hugo/deep-equal.js
@@ -0,0 +1,26 @@
+var deepEqual = function(a, b) {
+  if (a == b) {
+    return true;
+  } else {
+    if (a && b && typeof a == typeof b && typeof a == 'object' && a.length == b.length) {
+      for (var property in a) {
+        if (!deepEqual(a[property], b[property])) {
+          return false
+        }
+      }
+      return true;
+    } else {
+      return false
+    }
+
+  }
+
+}
+
+var a = {1:1, 2:2}
+var b = {1:1, 2:2}
+console.log('1 === 1: ', deepEqual(1,1))
+console.log(a,b)
+console.log(deepEqual(a,b))
+console.log('null === null: ' , deepEqual(null, null))
+console.log('null === a:', deepEqual(null, a))
