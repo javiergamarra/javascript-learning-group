8f1fa3527de12757f8521874672c517beb8ca7e0
Javier Gamarra <nhpatt@gmail.com>
2015-09-23 16:55:29 +0200

    better fizz buzz

--- 2th chapter/hugo/fizzbuzz.js
+++ 2th chapter/hugo/fizzbuzz.js
@@ -1,12 +1,10 @@
 //Exercise 2: Already with modifications
 function FizzBuzz() {
-    for (var i = 1; i < 101; i++) {
-        var divisibleBy3 = false;
-        var divisibleBy5 = false;
-        if (i % 3 == 0) divisibleBy3 = true;
-        if (i % 5 == 0) divisibleBy5 = true;
-        var strToPrint = "" + (divisibleBy3 ? "Fizz" : "") + (divisibleBy5 ? "Buzz" : "");
-        console.log(strToPrint.length == 0 ? i : strToPrint);
+    for (var i = 1; i <= 100; i++) {
+        var divisibleBy3 = i % 3 == 0;
+        var divisibleBy5 = i % 5 == 0;
+        var strToPrint = (divisibleBy3 ? "Fizz" : "") + (divisibleBy5 ? "Buzz" : "");
+        console.log(strToPrint || i);
     }
 }
 FizzBuzz();
