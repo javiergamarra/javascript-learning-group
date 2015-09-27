30477b2d3380c5a874d0c6d75569ec982ba8434a
Javier Gamarra <nhpatt@gmail.com>
2015-09-27 17:45:31 +0200

    simplify due to truthy and falsy, useless param & fix bug with odd sizes (shift + push does not work)

--- 2th chapter/jkappler/chess-v2.js
+++ 2th chapter/jkappler/chess-v2.js
@@ -1,19 +1,17 @@
 function printChessBoard(size) {
-    var arr = Array.apply(null, Array(size)).map(function (x, i) {
-            if (i % 2 == 0) {
-                return '#'
-            } else {
-                return ' '
-            }
-        }),
-        board = '',
-        removed;
+
+    var arr = Array.apply(null, new Array(size)).map(function (x, i) {
+        return i % 2 ? ' ' : '#';
+    });
+    var board = '';
 
     for (var i = 0; i < size; i++) {
-        arr.splice(0, 1);
-        arr.push(arr[1]);
+        arr.shift();
+        arr.push((arr.length + i) % 2 ? '#' : ' ');
         board += arr.join('') + '\n';
     }
 
     console.log(board);
 }
+
+printChessBoard(8);
