db6c458a3df1d0435dd3ded6bb8867aa23bbe799
Javier Gamarra <nhpatt@gmail.com>
2015-09-27 14:06:51 +0200

    could be simpler :)

--- 2th chapter/jkappler/chess-v1.js
+++ 2th chapter/jkappler/chess-v1.js
@@ -4,10 +4,12 @@ function printChessBoard(size) {
 
     for (var i = 0; i < size; i++) {
         for (var j = 0; j < size; j++) {
-            board += mark[((i + j) % 2) == 0 ? 0 : 1];
+            board += mark[(i + j) % 2];
         }
         board += '\n';
     }
 
     console.log(board);
 }
+
+printChessBoard(4);
