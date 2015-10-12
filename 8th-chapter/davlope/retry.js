fe2b5e38427d091c601c4d5371b638ae757b11c4
davlope <davlope@gmail.com>
2015-10-12 11:45:17 +0200

    Create retry.js

---
+++ 8th-chapter/davlope/retry.js
@@ -0,0 +1,33 @@
+function MultiplicatorUnitFailure(message) {
+  this.message = message;
+  this.stack = (new Error()).stack;
+}
+MultiplicatorUnitFailure.prototype = Object.create(Error.prototype);
+MultiplicatorUnitFailure.prototype.name = "MultiplicatorUnitFailure";
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.5)
+    return a * b;
+  else
+    throw new MultiplicatorUnitFailure("MultiplicatorUnitFailure");
+}
+
+function reliableMultiply(a, b) {
+  // Your code here.
+  try {
+    var result = primitiveMultiply(a,b);
+    if (result){
+      return result;
+    }
+  } catch (e) {
+    if (e instanceof MultiplicatorUnitFailure){
+      //console.log("Inernal error, retry...");
+      return reliableMultiply(a, b);
+    }else{
+      throw e;
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
