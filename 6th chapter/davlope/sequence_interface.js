23cf77efb54f856c6461c66d9791b990df3df5fe
davlope <davlope@gmail.com>
2015-10-12 11:19:04 +0200

    Update sequence_interface.js

--- 6th chapter/davlope/sequence_interface.js
+++ 6th chapter/davlope/sequence_interface.js
@@ -1,5 +1,50 @@
 // Your code here.
-//WIP :P
+function RangeSeq(start, end) {
+    this.counter = 0;
+    this.elements = new Array(end - start + 1).fill().map((x, i) => start + i);
+    this.limit = this.elements.length;
+}
+RangeSeq.prototype.next = function () {
+    if (this.hasNext()) {
+        console.log(this.elements[this.counter]);
+    }
+    this.counter += 1;
+};
+RangeSeq.prototype.hasNext = function () {
+    return this.counter < this.limit && this.counter < this.elements.length
+};
+RangeSeq.prototype.iterate = function (limit) {
+    this.limit = limit || this.elements.length;
+    while (this.hasNext()) {
+        this.next();
+    }
+};
+
+function ArraySeq(elements) {
+    this.counter = 0;
+    this.elements = elements;
+    this.limit = this.elements.length;
+}
+ArraySeq.prototype.next = function () {
+    if (this.hasNext()) {
+        console.log(this.elements[this.counter]);
+    }
+    this.counter += 1;
+};
+ArraySeq.prototype.hasNext = function () {
+    return this.counter < this.limit && this.counter < this.elements.length
+};
+ArraySeq.prototype.iterate = function (limit) {
+    this.limit = limit || this.elements.length;
+    while (this.hasNext()) {
+        this.next();
+    }
+};
+
+function logFive(seq) {
+    seq.iterate(5);
+}
+
 logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
