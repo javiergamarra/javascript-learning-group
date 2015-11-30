c5a28c6943a312d14157018daddb2384ef488bcf
davlope <davlope@gmail.com>
2015-11-30 18:55:11 +0100

    Create quoting_style.js

---
+++ 9th-chapter/davlope/quoting_style.js
@@ -0,0 +1,12 @@
+function verify(regexp, yes, no) {
+  // Ignore unfinished exercises
+  if (regexp.source == "...") return;
+  yes.forEach(function(s) {
+    if (!regexp.test(s))
+      console.log("Failure to match '" + s + "'");
+  });
+  no.forEach(function(s) {
+    if (regexp.test(s))
+      console.log("Unexpected match for '" + s + "'");
+  });
+}
